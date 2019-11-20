5025fdd79c838f2f849aa184b188bc7b4e563907
Bulat Kerimov <bulatker@gmail.com>
2019-11-20 16:34:50 +0100

    WIP

--- utils/utils_schema_DST.py
+++ utils/utils_schema_DST.py
@@ -1,6 +1,8 @@
 import os
 from os.path import dirname, join
 import json
+from utils.utils_multiWOZ_DST import Lang
+from utils.config import *
 
 souce_dir = dirname(dirname(__file__))
 dataset_dir = join(souce_dir, 'data/schema/')
@@ -23,10 +25,10 @@ def write_ontology_file():
     for file in schema_files:
         with open(file) as f:
             dials = json.load(f)
-            [domains.append(service["service_name"].split("_")[0].lower()) for service in dials if
+            [domains.append(get_domain_name(service["service_name"])) for service in dials if
              service["service_name"] not in domains]
             domain_slot_values = {
-                str(service["service_name"].split("_")[0].lower() + "-" + slot["name"]): slot["possible_values"]
+                str(get_domain_name(service["service_name"].split("_")) + "-" + slot["name"]): slot["possible_values"]
                 for service in dials for slot in service["slots"]}
     with open(join(dataset_dir, 'ontology.json'), 'w') as j:
         json.dump(domain_slot_values, j, indent=4)
@@ -55,28 +57,27 @@ def process_dialogue(dialogue_dict, SLOTS):
 
     def combine_turns(user_turn, system_turn):
         assert user_turn["speaker"] == "USER" and system_turn["speaker"] == "SYSTEM"
-        assert len(user_turn["frames"]) == 1
-        user_state = user_turn["frames"][0]["state"]
-        turn_domain = user_turn["frames"][0]["service"].split("_")[0].lower()
-        turn_belief = ["-".join([turn_domain, slot, value]) for slot, values in user_state["slot_values"].items() for
-                       value in values]
-        turn_utterance = system_turn["utterance"] + " ; " + user_turn["utterance"]
-
-        slots_to_idx = {slot: SLOTS.index("-".join([turn_domain, slot])) for slot in user_state["slot_values"].keys()}
+        turn_domain = get_domain_name(user_turn["frames"][-1]["service"])
+        turn_belief = ["-".join([get_domain_name(frame["service"]), slot, value]) for frame in user_turn["frames"] for
+                       slot, values in
+                       frame["state"]["slot_values"].items() for value in values]
+        turn_utterance = system_turn["utterance"].lower() + " ; " + user_turn["utterance"].lower() + " "
 
         gating_label = [2] * len(SLOTS)
         generate_y = ['none'] * len(SLOTS)
 
-        for slot, idx in slots_to_idx.items():
-            if user_state["slot_values"][slot] == "dontcare":
-                gating_label[idx] = 1
-                generate_y[idx] = "dontcare"
-            elif user_state["slot_values"][slot] == "No":
-                gating_label[idx] = 2
-                generate_y[idx] = "none"
+        for elem in turn_belief:
+            [domain, slot, value] = elem.split("-", 2)
+            slot_idx = SLOTS.index("-".join([domain, slot]))
+            if value == "dontcare":
+                gating_label[slot_idx] = 1
+                generate_y[slot_idx] = "dontcare"
+            elif value == "No":
+                gating_label[slot_idx] = 2
+                generate_y[slot_idx] = "none"
             else:
-                gating_label[idx] = 0
-                generate_y[idx] = user_state["slot_values"][slot]
+                gating_label[slot_idx] = 0
+                generate_y[slot_idx] = value
 
         return {'turn_domain': turn_domain,
                 'turn_utterance': turn_utterance,
@@ -85,27 +86,30 @@ def process_dialogue(dialogue_dict, SLOTS):
                 'gating_label': gating_label}
 
     idx = dialogue_dict["dialogue_id"]
-    domains = [service.split("_")[0].lower() for service in dialogue_dict["services"]]
+    domains = [get_domain_name(service) for service in dialogue_dict["services"]]
     initial_turn = [{"speaker": "SYSTEM",
                      "utterance": ""}]
     dialogue_dict["turns"] = initial_turn + dialogue_dict["turns"]
-    dialogue_history = " ; "
+    dialogue_history = ""
     turn_id = 0
     data = []
     assert initial_turn[-1]["speaker"] == "SYSTEM"
 
     for system_turn, user_turn in pairwise(dialogue_dict["turns"]):
-        print(turn_id)
         new_dict = combine_turns(user_turn, system_turn)
         dialogue_history = dialogue_history + new_dict["turn_utterance"]
-        new_dict["dialog_history"] = dialogue_history + new_dict["turn_utterance"]
+        new_dict["dialog_history"] = dialogue_history.strip()
+        new_dict["turn_utterance"] = new_dict["turn_utterance"].strip()
         new_dict["ID"] = idx
         new_dict["turn_id"] = turn_id
         turn_id += 1
         new_dict["domains"] = domains
         data.append(new_dict)
 
-    return data
+    assert len(data) > 1
+    dialog_length = len(data[-1]["dialogue_history"])
+
+    return data, dialog_length
 
 
 def pairwise(iterable):
@@ -113,8 +117,67 @@ def pairwise(iterable):
     return zip(*[iter(iterable)] * 2)
 
 
-SLOTS = read_ontology_file("/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/ontology.json")
-with open("/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/train/dialogues_123.json") as f:
-    dialogue_dict = json.load(f)[0]
-    data = process_dialogue(dialogue_dict, list(SLOTS[2]))
-    print()
+def get_domain_name(service_string):
+    """
+    Processes the name of the service in the schema dialogue format
+    :param service_string:
+    :return: string of a domain, lowered
+    """
+    return service_string.split("_")[0].lower()
+
+
+def process_all_dialogues(directory, SLOTS):
+    """
+    Processes all dialogues in the folder and combines them into one "data" dictionary
+    :param directory: destination of the folder with .json dictinaries of dialogues
+    :param SLOTS: all possible slots from the ontology
+    :return: data dictionary, compatible with the one in the multiWOZ file
+    """
+    file_paths = []
+    for _, __, filenames in os.walk(directory):
+        file_paths.extend([join(directory, filename) for filename in filenames if filename != "schema.json"])
+        break
+    data = []
+    max_resp_len = 0
+
+    for file_path in file_paths:
+        with open(file_path) as f:
+            dialogue_dicts = json.load(f)
+            for dialogue_dict in dialogue_dicts:
+                temp_data, dialogue_length = process_dialogue(dialogue_dict, SLOTS)
+                data.extend(temp_data)
+                max_resp_len = dialogue_length if dialogue_length > max_resp_len else max_resp_len
+    return data, max_resp_len
+
+
+def prepare_data_seq(training, batch_size=100):
+    # TODO Make it os path related
+    ontology_dir = "/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/ontology.json"
+    train_dialogues_dir = "/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/train"
+    test_dialogues_dir = "/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/test"
+    dev_dialogues_dir = "/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/dev"
+
+    ontology, domains, ALL_SLOTS = read_ontology_file(ontology_dir)
+    lang, mem_lang = Lang(), Lang()
+    lang.index_words(ALL_SLOTS, 'slot')
+    mem_lang.index_words(ALL_SLOTS, 'slot')
+    lang_name = 'lang-all.pkl' if args["all_vocab"] else 'lang-train.pkl'
+    mem_lang_name = 'mem-lang-all.pkl' if args["all_vocab"] else 'mem-lang-train.pkl'
+
+    if training:
+        pair_train, train_max_len = process_all_dialogues(train_dialogues_dir)
+        pair_test, test_max_len = process_all_dialogues(test_dialogues_dir)
+        pair_dev, dev_max_len = process_all_dialogues(test_dialogues_dir)
+
+
+
+
+SLOTS = read_ontology_file("/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/ontology.json")[2]
+directory = "/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/train"
+data = process_all_dialogues(directory, SLOTS)
+
+# with open("/Users/khasbulat.kerimov/Projects/trade-dst/data/schema/train/dialogues_123.json") as f:
+#     dialogue_dict = json.load(f)[6]
+#     data = process_dialogue(dialogue_dict, list(SLOTS[2]))
+
+print()
